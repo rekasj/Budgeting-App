086c5dbaaaac5aa5cacfd211bd0db7ab48a35649
jakub_rekas <jakub.rekas@bitcomp.fi>
2023-09-17 16:57:59 +0200

    Initial Commit

---
+++ Budgeting App/Budgeting_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budgeting_AppApp.swift
+//  Budgeting App
+//
+//  Created by Jakub Rękas on 17/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Budgeting_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budgeting App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Budgeting App
+//
+//  Created by Jakub Rękas on 17/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
